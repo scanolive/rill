11cc8bd53560de7816ba2d1d3bdbd923cf045656
rill <scani@163.com>
2020-06-16 14:34:29 +0800

    fix grep -q

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 python -c 'import MySQLdb' > /dev/null 2>&1
 if [[ $? -ne 0 ]];then
 	echo "module MySQLdb not install"
-	if grep -q debian /etc/issue;then
+	if grep -iq debian /etc/issue;then
 		echo "apt-get install python-mysqldb"	  	
 	fi
 fi
